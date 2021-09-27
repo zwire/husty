c5a087cb4ad8cd2579495abaf9ac52f469ef33fe
husty530 <husty530@gmail.com>
2021-09-27 23:04:53 +0900

    *

--- Lib/cs/Husty/IO/SerialPort.cs
+++ Lib/cs/Husty/IO/SerialPort.cs
@@ -48,17 +48,14 @@ namespace Husty.IO
 
         public BidirectionalDataStream GetStream()
         {
-            return GetStreamAsync().Result;
+            _connectionTask.Wait();
+            var stream = _port.BaseStream;
+            return new BidirectionalDataStream(stream, stream, WriteTimeout, ReadTimeout);
         }
 
         public async Task<BidirectionalDataStream> GetStreamAsync()
         {
-            return await Task.Run(() =>
-            {
-                _connectionTask.Wait();
-                var stream = _port.BaseStream;
-                return new BidirectionalDataStream(stream, stream, WriteTimeout, ReadTimeout);
-            });
+            return await Task.Run(() => GetStream());
         }
         
         public void Dispose()
