16746556da826cf57e3efaf6dc903c563a34e675
husty530 <husty530@gmail.com>
2021-11-11 14:08:50 +0900

    *

---
+++ Lib/cs/Husty/Filter/FirFilter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Husty.Filter
+{
+    /// <summary>
+    /// Finite Impulse Response
+    /// See http://www.densikairo.com/Development/Public/study_dsp/464952A5D5A5A3A5EBA5BFA4CBB4D8A4B7A4C6.html
+    /// </summary>
+    public class FirFilter
+    {
+
+        // ------- Fields ------- //
+
+        private int _count;
+        private readonly double[] _buffer;
+        private readonly double[] _weightTable;
+
+
+        // ------- Constructor ------- //
+
+        public FirFilter(int size)
+        {
+            if (size < 1) throw new ArgumentOutOfRangeException("Require: size > 0");
+            _buffer = new double[size];
+            _weightTable = new double[size];
+            var one = 1.0 / size;
+            for (int i = 0; i < size; i++)
+                _weightTable[i] = one;
+        }
+
+        public FirFilter(double[] weightTable)
+        {
+            _buffer = new double[weightTable.Length];
+            _weightTable = weightTable;
+        }
+
+
+        // -------- Methods ------- //
+
+        public double Update(double measurementValue)
+        {
+            var len = _buffer.Length;
+            for (int i = 0; i < len - 1; i++)
+                _buffer[i] = _buffer[i + 1];
+            _buffer[^1] = measurementValue;
+            if (++_count < len) return measurementValue;
+            if (_count % len is 0) _count = len;
+
+            var value = 0.0;
+            for (int i = 0; i < len; i++)
+                value += _buffer[i] * _weightTable[i];
+            return value;
+        }
+
+    }
+}
---
+++ Lib/cs/Husty/Filter/MedianFilter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace Husty.Filter
+{
+    public class MedianFilter
+    {
+
+        // ------- Fields ------- //
+
+        private int _count;
+        private readonly double[] _buffer;
+
+
+        // ------- Constructor -------- //
+
+        public MedianFilter(int size)
+        {
+            if (size < 1) throw new ArgumentOutOfRangeException("Require: size > 0");
+            _buffer = new double[size];
+        }
+
+
+        // ------- Methods ------- //
+
+        public double Update(double measurementValue)
+        {
+            var len = _buffer.Length;
+            _buffer[_count++ % len] = measurementValue;
+            if (_count < len) return measurementValue;
+            if (_count % len is 0) _count = len;
+
+            var val = _buffer.OrderBy(x => x).ToArray();
+            if (len % 2 is 0)
+                return (val[len / 2 - 1] + val[len / 2]) / 2;
+            else
+                return val[len / 2];
+        }
+
+    }
+}
