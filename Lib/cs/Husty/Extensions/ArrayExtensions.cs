c3328196feedb2159debf91c701b606af956a39b
husty530 <husty530@gmail.com>
2021-11-15 17:21:47 +0900

    *

--- Lib/cs/Husty/Extensions/ArrayExtensions.cs
+++ Lib/cs/Husty/Extensions/ArrayExtensions.cs
@@ -1,44 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Husty
 {
     public static class ArrayExtensions
     {
 
-        public static T ArgMax<T>(this T[] array, out int index) where T: IConvertible, IComparable
+        public static double ArgMax<T>(this IEnumerable<T> src, out int index) where T: struct, IConvertible, IComparable
         {
-            if (double.TryParse((string)(object)array[0], out var _))
+            if (double.TryParse((string)(object)src.FirstOrDefault(), out var _))
                 throw new ArgumentException();
             var max = double.MinValue;
             index = 0;
-            for (int i = 0; i < array.Length; i++)
+            int i = 0;
+            foreach (var s in src)
             {
-                var num = (double)(object)array[i];
+                var num = (double)(object)s;
                 if (num > max)
                 {
                     max = num;
                     index = i;
                 }
+                i++;
             }
-            return (T)(object)max;
+            return max;
         }
 
-        public static T ArgMin<T>(this T[] array, out int index) where T : IConvertible, IComparable
+        public static double ArgMin<T>(this IEnumerable<T> src, out int index) where T : struct, IConvertible, IComparable
         {
-            if (double.TryParse((string)(object)array[0], out var _))
+            if (double.TryParse((string)(object)src.FirstOrDefault(), out var _))
                 throw new ArgumentException();
             var min = double.MaxValue;
             index = 0;
-            for (int i = 0; i < array.Length; i++)
+            int i = 0;
+            foreach (var s in src)
             {
-                var num = (double)(object)array[i];
+                var num = (double)(object)s;
                 if (num < min)
                 {
                     min = num;
                     index = i;
                 }
+                i++;
             }
-            return (T)(object)min;
+            return min;
+        }
+
+        public static double Median<T>(this IEnumerable<T> src) where T : struct, IComparable<T>, IEquatable<T>
+        {
+            if (!src.Any()) throw new InvalidOperationException("Cannot compute median for an empty set.");
+            var doubleArray = src.Select(a => Convert.ToDouble(a)).OrderBy(x => x).ToArray();
+            var len = doubleArray.Length;
+            var odd = len % 2 is not 0;
+            var median = odd ? doubleArray[len / 2] : (doubleArray[len / 2 - 1] + doubleArray[len / 2]) / 2.0;
+            return median;
+        }
+
+        public static double Variance<T>(this IEnumerable<T> src) where T : struct, IComparable<T>, IEquatable<T>
+        {
+            if (!src.Any()) throw new InvalidOperationException("Cannot compute median for an empty set.");
+            var doubleArray = src.Select(a => Convert.ToDouble(a)).ToArray();
+            var mean = doubleArray.Average();
+            var sum2 = doubleArray.Select(a => a * a).Sum();
+            var variance = sum2 / doubleArray.Length - mean * mean;
+            return variance;
+        }
+
+        public static double StdDev<T>(this IEnumerable<T> src)
+            where T : struct, IComparable<T>, IEquatable<T>
+        {
+            return Math.Sqrt(Variance(src));
         }
 
     }
---
+++ Lib/cs/Husty/Extensions/MathExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Husty
+{
+    public static class MathExtensions
+    {
+
+        public static T Inf<T>(this T value, T min)
+             where T : struct, IComparable<T>, IEquatable<T>
+            => value.CompareTo(min) < 0 ? min : value;
+
+        public static T Sup<T>(this T value, T max)
+             where T : struct, IComparable<T>, IEquatable<T>
+            => value.CompareTo(max) > 0 ? max : value;
+
+        public static T InsideOf<T>(this T value, T min, T max)
+             where T : struct, IComparable<T>, IEquatable<T>
+        {
+            if (min.CompareTo(max) > 0) throw new ArgumentException("Min > Max !");
+            value = value.CompareTo(min) < 0 ? min : value;
+            value = value.CompareTo(max) > 0 ? max : value;
+            return value;
+        }
+
+    }
+}
