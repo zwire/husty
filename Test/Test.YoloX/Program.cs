d42c37113c3610c2b9eea555c8dcd9918d0a577a
husty530 <husty530@gmail.com>
2021-11-22 21:39:17 +0900

    yolox

--- Test/Test.YoloX/Program.cs
+++ Test/Test.YoloX/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using OpenCvSharp;
 
 namespace Test.YoloX
@@ -8,19 +10,21 @@ namespace Test.YoloX
     {
         static void Main(string[] args)
         {
-
-            using var frame = new Mat("..\\..\\..\\dog1.jpg");
-            var yolox = new YoloX("..\\..\\..\\yolox_tiny.onnx");
+            var frame = new Mat("..\\..\\..\\dog3.jpg");
+            var yolox = new YoloX("..\\..\\..\\yolox_tiny.onnx", 0.5);
+            var names = File.ReadAllText("..\\..\\..\\_.names").Split("\n").Select(t => t.TrimEnd()).ToArray();
+            var colors = Enumerable.Range(0, 80).Select(_ => new Scalar(new Random().Next(255), new Random().Next(255), new Random().Next(255))).ToArray();
             var watch = new Stopwatch();
             watch.Start();
             var results = yolox.Run(frame);
-            watch.Stop();
-            Console.WriteLine(watch.ElapsedMilliseconds);
             foreach (var r in results)
             {
-                Console.WriteLine($"class: {r.Label}, x: {r.Box.X}, y: {r.Box.Y}, w: {r.Box.Width}, h: {r.Box.Height}");
-                Cv2.Rectangle(frame, r.Box, new(0, 0, 255), 2);
+                Console.WriteLine($"{r.Probability * 100:f0}% {names[r.Label]}, x: {r.Box.X}, y: {r.Box.Y}");
+                Cv2.Rectangle(frame, r.Box, colors[r.Label], 1);
             }
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds);
+            Cv2.Resize(frame, frame, new(frame.Width * 2, frame.Height * 2));
             Cv2.ImShow(" ", frame);
             Cv2.WaitKey();
         }
--- Test/Test.YoloX/YoloX.cs
+++ Test/Test.YoloX/YoloX.cs
@@ -9,45 +9,84 @@ using OpenCvSharp;
 namespace Test.YoloX
 {
 
-    public class YoloX : OnnxBase<Mat, IEnumerable<(int Label, Rect Box)>>
+    public record YoloXResult(Rect Box, double Probability, int Label);
+
+    public class YoloX : OnnxBase<Mat, IEnumerable<YoloXResult>>
     {
 
-        public YoloX(string model) : base(model, Provider.CPU, OptimizationLevel.Off) { }
+        private const int _classCount = 80;
+        private readonly int[] _strides = new[] { 8, 16, 32 };
+        private readonly Size _size;
+        private readonly int _detectionCount;
+        private readonly double _confThresh;
+        private readonly Mat _paddingBase;
 
-
-        public override IEnumerable<(int Label, Rect Box)> Run(Mat frame)
+        public YoloX(string model, double confidenceThreshold = 0.5) 
+            : base(model, Provider.CPU, OptimizationLevel.Off) 
         {
-            Cv2.Resize(frame, frame, new Size(416, 416));
-            var xRatio = frame.Width / 416.0;
-            var yRatio = frame.Height / 416.0;
-            using var frame2 = frame.CvtColor(ColorConversionCodes.BGR2RGB).Resize(new Size(416, 416));
-            frame2.ConvertTo(frame2, MatType.CV_32FC3);
-            var inputArray = frame2.To1DFloatArray();
-            var outputArray = base.Run(inputArray)["output"].To2DJaggedArray(3549, 85);
+            if (model.Contains("tiny") || model.Contains("nano"))
+                _size = new(416, 416);
+            else
+                _size = new(640, 640);
+            _confThresh = confidenceThreshold;
+            _detectionCount = _strides.Select(x => _size.Width / x * _size.Height / x).Sum();
+            _paddingBase = new(_size.Height, _size.Width, MatType.CV_8UC3, new(114, 114, 114));
+        }
+
+
+        public override IEnumerable<YoloXResult> Run(Mat frame)
+        {
+            var ratio = 1.0 / Math.Min((double)_size.Width / frame.Width, (double)_size.Height / frame.Height);
+            var padded = _paddingBase.Clone();
+            var size = new Size(frame.Width / ratio, frame.Height / ratio);
+            padded[new Rect(0, 0, size.Width, size.Height)] = frame.Resize(size);
+            var inputArray = ToInputArray(padded);
+            var outputArray = base.Run(inputArray)["output"].To2DJaggedArray(_detectionCount, _classCount + 5);
 
             foreach (var stride in new[] { 8, 16, 32 })
             {
-                var xSize = 416 / stride;
-                var ySize = 416 / stride;
-                for (int i = 0; i < ySize; i++)
+                var scale = stride * ratio;
+                var xGrids = padded.Width / stride;
+                var yGrids = padded.Height / stride;
+                for (int i = 0; i < yGrids; i++)
                 {
-                    for (int j = 0; j < xSize; j++)
+                    for (int j = 0; j < xGrids; j++)
                     {
-                        var r = outputArray[i * xSize + j];
+                        var r = outputArray[i * xGrids + j];
                         var index = r.Skip(5).ToArray().ArgMax(out var prob);
-                        if (r[4] * prob > 0.01)
+                        if (r[4] > _confThresh)
                         {
-                            var x = (r[0] + j) * stride * xRatio;
-                            var y = (r[1] + i) * stride * yRatio;
-                            var w = Math.Exp(r[2]) * stride * xRatio;
-                            var h = Math.Exp(r[3]) * stride * yRatio;
-                            yield return(index, new Rect2d(x - w / 2, y - h / 2, w, h).ToRect());
+                            var x = r[0] + j;
+                            var y = r[1] + i;
+                            var w = Math.Exp(r[2]);
+                            var h = Math.Exp(r[3]);
+                            var box = new Rect2d(x - w / 2, y - h / 2, w, h).Scale(scale, scale).ToRect();
+                            yield return new(box, prob, index);
                         }
                     }
                 }
             }
 
         }
+
+        private unsafe float[] ToInputArray(Mat frame)
+        {
+            var frame2 = frame.CvtColor(ColorConversionCodes.BGR2RGB);
+            var d = frame2.DataPointer;
+            var len = frame2.Rows * frame2.Cols;
+            var array = new float[len * 3];
+            for (int i = 0; i < len; i++)
+            {
+                array[i + len * 0] = d[i * 3 + 0];
+                array[i + len * 1] = d[i * 3 + 1];
+                array[i + len * 2] = d[i * 3 + 2];
+            }
+            return array;
+        }
+
+        //private float[] mean = new[] { 255f * 0.485f, 255f * 0.456f, 255f * 0.406f };
+
+        //private float[] scale = new[] { 1 / (255f * 0.229f), 1 / (255f * 0.224f), 1 / (255f * 0.225f) };
 
     }
 }
