042f3207d4bcec59e0eabfa65e7a7c2d530c91e1
husty530 <husty530@gmail.com>
2022-01-11 19:18:03 +0900

    *

--- Tutorial/Tutorial.Yolo/Program.cs
+++ Tutorial/Tutorial.Yolo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using OpenCvSharp;
 using Husty.OpenCvSharp;
+using System.Diagnostics;
 
 namespace Tutorial.Yolo
 {
@@ -13,17 +15,24 @@ namespace Tutorial.Yolo
             var cfg = "..\\..\\..\\model\\_.cfg";
             var weights = "..\\..\\..\\model\\_.weights";
             var names = "..\\..\\..\\model\\_.names";
+            var onnx = "..\\..\\..\\model\\yolox_tiny.onnx";
 
             // initialize detector.
             // blob width and height must be multiple of 32.
-            var detector = new Yolov3v4(cfg, weights, names, new Size(384, 256));
+            var detector = new YoloX(onnx, names);
+            //var detector = new Yolov3v4(cfg, weights, names, new Size(384, 256));
+            var colors = Enumerable.Range(0, 80).Select(_ => new Scalar(new Random().Next(255), new Random().Next(255), new Random().Next(255))).ToArray();
 
             // input image path
-            var img = Cv2.ImRead("..\\..\\..\\sample.jpg");
+            var img = Cv2.ImRead("..\\..\\..\\sample1.jpg");
 
             // execute inference session
+            detector.Run(img); // initialize?
+            var watch = new Stopwatch();
+            watch.Start();
             var results = detector.Run(img);
-            detector.Run(img);
+            watch.Stop();
+            Console.WriteLine(watch.ElapsedMilliseconds + " ms");
 
             // show results
             foreach (var r in results)
