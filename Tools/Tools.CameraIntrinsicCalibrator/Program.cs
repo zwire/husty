6f362a9af27fae2f64be6b58c8e05f61a2a15bdd
husty530 <husty530@gmail.com>
2022-01-15 17:54:24 +0900

    *

---
+++ Tools/Tools.CameraIntrinsicCalibrator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+using ConsoleAppFramework;
+using Microsoft.Extensions.Hosting;
+using Husty.OpenCvSharp;
+
+namespace Tools.CameraIntrinsicCalibrator
+{
+    internal class Program : ConsoleAppBase
+    {
+
+        static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder().RunConsoleAppFrameworkAsync<Program>(args);
+        }
+
+        public void Run(
+            [Option("d", "working directory")] string dir = null
+        )
+        {
+            dir ??= Environment.CurrentDirectory;
+            var files = Directory.GetFiles(dir, "*.png");
+            var param = IntrinsicCameraCalibrator.CalibrateWithChessboardImages(new(7, 10, 32.5f), files);
+            param.Save($"{dir}\\intrinsic.json");
+            Console.WriteLine("successfully calibrated and saved as intrinsic.json");
+        }
+    }
+}
+
