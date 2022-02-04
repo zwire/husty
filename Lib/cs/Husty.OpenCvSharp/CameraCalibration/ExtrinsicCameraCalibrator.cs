0cd8c6204442a5088acb9596b5515f4a6abad0a0
husty530 <husty530@gmail.com>
2022-02-04 20:41:52 +0900

    *

--- Lib/cs/Husty.OpenCvSharp/CameraCalibration/ExtrinsicCameraCalibrator.cs
+++ Lib/cs/Husty.OpenCvSharp/CameraCalibration/ExtrinsicCameraCalibrator.cs
@@ -35,5 +35,40 @@ namespace Husty.OpenCvSharp
             return new ExtrinsicCameraParameters(rotationMat, translationVec);
         }
 
+        public unsafe static ExtrinsicCameraParameters CalibrateWithCorrespondencePoint3D(List<(Point3d, Point3d)> points)
+        {
+            if (points.Count is 0)
+                throw new ArgumentException("Require: points count >= 1");
+            using var referenceMat = new Mat(points.Count, 4, MatType.CV_64F, 0);
+            using var targetMat = new Mat(points.Count, 4, MatType.CV_64F, 0);
+            var rd = (double*)referenceMat.Data;
+            var td = (double*)targetMat.Data;
+            for (int i = 0; i < points.Count; i++)
+            {
+                rd[i * 4 + 0] = points[i].Item1.X;
+                rd[i * 4 + 1] = points[i].Item1.Y;
+                rd[i * 4 + 2] = points[i].Item1.Z;
+                rd[i * 4 + 3] = 1;
+                td[i * 4 + 0] = points[i].Item2.X;
+                td[i * 4 + 1] = points[i].Item2.Y;
+                td[i * 4 + 2] = points[i].Item2.Z;
+                td[i * 4 + 3] = 1;
+            }
+            using var refPinv = referenceMat.Inv(DecompTypes.SVD);
+            using var m = (Mat)(refPinv * targetMat);
+            var md = (double*)m.Data;
+            var rotMat = new Mat(3, 3, MatType.CV_64F, new[]
+            {
+                md[0], md[1], md[2],
+                md[4], md[5], md[6],
+                md[8], md[9], md[10]
+            });
+            var trsVec = new Mat(3, 1, MatType.CV_64F, 0);
+            trsVec.At<double>(0, 0) = md[3];
+            trsVec.At<double>(1, 0) = md[7];
+            trsVec.At<double>(2, 0) = md[11];
+            return new(rotMat, trsVec);
+        }
+
     }
 }
--- Lib/cs/Husty.OpenCvSharp/CameraCalibration/ExtrinsicCameraParameters.cs
+++ Lib/cs/Husty.OpenCvSharp/CameraCalibration/ExtrinsicCameraParameters.cs
@@ -25,7 +25,16 @@ namespace Husty.OpenCvSharp
 		public ExtrinsicCameraParameters Clone()
 			=> new(RotationMatrix, TranslationVector);
 
-        public void Save(string fileName)
+		public unsafe Vec3d GetEulerCoordinate()
+		{
+			var rd = (double*)RotationMatrix.Data;
+			var x = Math.Atan2(rd[7], rd[8]);
+			var y = Math.Asin(-rd[6]);
+			var z = Math.Atan2(rd[3], rd[0]);
+			return new(x, y, z);
+		}
+
+		public void Save(string fileName)
         {
             var obj = new ExtrinsicJson(this);
             File.WriteAllText(fileName, obj.Serialize());
