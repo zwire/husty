a416ccd8a211e0fae73714b1832ebb90c5132eff
Takumi Yamashita <64026753+husty530@users.noreply.github.com>
2024-05-01 17:31:54 +0900

    overall update

---
+++ examples/Tracker/Program.cs
@@ -0,0 +1,41 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Tracking;
+
+// open camera
+using var cap = new VideoCapture(0);
+using var frame = new Mat();
+
+// initialize tracker
+using var tracker = TrackerCSRT.Create();
+
+// loop
+var box = new Rect();
+while (cap.Read(frame))
+{
+  if (Console.KeyAvailable)
+  {
+    var key = Console.ReadKey().Key;
+    if (key is ConsoleKey.Enter)
+    {
+      // input user-defined box
+      box = Cv2.SelectROI(frame);
+      tracker.Init(frame, box);
+      Cv2.DestroyAllWindows();
+    }
+    else if (key is ConsoleKey.Escape)
+    {
+      break;
+    }
+  }
+
+  // check if box exists
+  if (box.Size != new Size())
+    tracker.Update(frame, ref box);
+
+  // visualize
+  Cv2.Rectangle(frame, box, new(0, 0, 255), 2);
+  Cv2.ImShow(" ", frame);
+  Cv2.WaitKey(1);
+
+}
+Console.WriteLine("completed");
